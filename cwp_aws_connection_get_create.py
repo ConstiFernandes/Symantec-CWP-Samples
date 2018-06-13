0d83c29d6d6b1203bd7dbe5da6dbecb6b3f20e0a
Constancio <33767222+ConstiF@users.noreply.github.com>
2018-06-13 14:32:37 -0700

    Update cwp_aws_connection_get_create.py
    Updated script for making it compatible with Python3

--- cwp_aws_connection_get_create.py
+++ cwp_aws_connection_get_create.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#
+# 
 # Copyright 2018 Symantec Corporation. All rights reserved.
 #
 #Script to automate list down AWS connection availabe for the customer and also can create connection for customer.
@@ -33,7 +33,7 @@ def getconnection():
   #Add to payload and header your CWP tenant & API keys - client_id, client_secret, x-epmp-customer-id and x-epmp-domain-id
   payload = {'client_id' : clientID, 'client_secret' : clientsecret}
   header = {"Content-type": "application/json" ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
-  response = requests.post(url, data=json.dumps(payload), headers=header) 
+  response = requests.post(url, data=json.dumps(payload), headers=header)
   authresult=response.status_code
   token=response.json()
   if (authresult!=200) :
@@ -52,39 +52,39 @@ def getconnection():
   output = {}
   output = response.json()
   #print output
-  print "\n List of available connections \n"
-  print output
+  print ("\n List of available connections \n")
+  print (output)
 
 def createconnection():
- 	token = {}
- 	mydict = {}
- #CWP REST API endpoint URL for auth function
-  	url = 'https://scwp.securitycloud.symantec.com/dcs-service/dcscloud/v1/oauth/tokens'
- #Add to payload and header your CWP tenant & API keys - client_id, client_secret, x-epmp-customer-id and x-epmp-domain-id
- 	payload = {'client_id' : clientID, 'client_secret' : clientsecret}
-  	header = {"Content-type": "application/json" ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
-  	response = requests.post(url, data=json.dumps(payload), headers=header)
-  	authresult=response.status_code
-  	token=response.json()
-  	if (authresult!=200) :
-    		print ("\nAuthentication Failed. Did you replace the API keys in the code with your CWP API Keys? Check clientsecret, clientID, customerID, and domainID\n")
-    		exit()
-	print "\nCreating connection \n"
+  token = {}
+  mydict = {}
+  #CWP REST API endpoint URL for auth function
+  url = 'https://scwp.securitycloud.symantec.com/dcs-service/dcscloud/v1/oauth/tokens'
+  #Add to payload and header your CWP tenant & API keys - client_id, client_secret, x-epmp-customer-id and x-epmp-domain-id
+  payload = {'client_id' : clientID, 'client_secret' : clientsecret}
+  header = {"Content-type": "application/json" ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
+  response = requests.post(url, data=json.dumps(payload), headers=header)
+  authresult=response.status_code
+  token=response.json()
+  if (authresult!=200) :
+    print ("\nAuthentication Failed. Did you replace the API keys in the code with your CWP API Keys? Check clientsecret, clientID, customerID, and domainID\n")
+    exit()
+  print ("\nCreating connection \n")
   #Extracting auth token
-  	accesstoken= token['access_token']
-  	accesstoken = "Bearer " + accesstoken
-	createurl = "https://scwp.securitycloud.symantec.com/dcs-service/dcscloud/v1/cpif/cloud_connections"
-	createpayload = {'client_id' : clientID, 'client_secret' : clientsecret}
-        createheader = {"Authorization": accesstoken ,"Content-type": "application/json" ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
- 	header1 = {"Content-type": "application/json","Authorization": accesstoken ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}	
-        createresponse = requests.post(createurl, data=json.dumps(createpayload), headers=header1)
-	mydict = createresponse.json()
-	print mydict
- 	print "\n External_ID : " + mydict[u'external_id'] 
-	print "\n Connection ID : " + mydict[u'id']
-	print "\nUse above information to update connection by running script cwp_aws_connection_update.py \n"
-if __name__=="__main__":
+  accesstoken= token['access_token']
+  accesstoken = "Bearer " + accesstoken
+  createurl = "https://scwp.securitycloud.symantec.com/dcs-service/dcscloud/v1/cpif/cloud_connections"
+  createpayload = {'client_id' : clientID, 'client_secret' : clientsecret}
+  createheader = {"Authorization": accesstoken ,"Content-type": "application/json" ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
+  header1 = {"Content-type": "application/json","Authorization": accesstoken ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
+  createresponse = requests.post(createurl, data=json.dumps(createpayload), headers=header1)
+  mydict = createresponse.json()
+  print (mydict)
+  print ("\n External_ID : " + mydict[u'external_id'])
+  print ("\n Connection ID : " + mydict[u'id'])
+  print ("\nUse above information to update connection by running script cwp_aws_connection_update.py \n")
 
+if __name__=="__main__":
    if (len(sys.argv) < 5):
       print ("Insufficient number of arguments passed. Pass all 4 CWP API key parameters from 'Setting Page->API Keys' tab. Usage: python cwp_aws_connection_get_create.py <Customer ID> <Domain ID> <Client Id> <Client Secret Key>")
       exit()
